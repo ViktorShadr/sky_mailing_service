1610580951b2d27b20a4e5ceecb54b68f4f93c54
Viktor Shadrin <v.viktor.shadrin@gmail.com>
2025-12-01 14:12:48 +0300

    feat: add new mailing management templates
    - Added templates:
      * mailing_list.html - mailing list
      * message_list.html - message list
      * recipient_list.html - recipient list
    
    - Implemented recipient management functionality:
      * Added RecipientListView to views.py
      * Configured /recipient/ URL route
      * Updated navigation menu
      * Implemented pagination for recipient list
      * Added empty list handling

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from mailing.views import MailingTemplateView
+from mailing.views import MailingTemplateView, RecipientListView
 
 app_name = 'mailing'
 
 urlpatterns = [
     path('', MailingTemplateView.as_view(), name='index'),
+    path('recipient/', RecipientListView.as_view(), name='recipient'),
+    # path('message/', MessageTemplateView.as_view(), name='message'),
+    # path('mailing/', MailingListTemplateView.as_view(), name='mailing'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView
 
 from mailing.models import Mailing, Client
 
@@ -12,5 +13,25 @@ class MailingTemplateView(TemplateView):
         context['active_mailings'] = Mailing.objects.filter(status='started').count()
         context['unique_clients'] = Client.objects.count()
         return context
+
+
+class RecipientListView(LoginRequiredMixin,ListView):
+    model = Client
+    template_name = "mailing/recipient_list.html"
+    context_object_name = "clients"
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Client.objects.all()
+
+
+
+# class MessageTemplateView(TemplateView):
+#     template_name = "mailing/message_list.html"
+#
+#
+# class MailingListTemplateView(TemplateView):
+#     template_name = "mailing/mailing_list.html"
+
 
 
