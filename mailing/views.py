46186eb3032191d9afa309c0dd39388487e58686
Viktor Shadrin <v.viktor.shadrin@gmail.com>
2025-12-02 11:25:35 +0300

    Added mailing statistics and improved comments
    - Added get_context_data method to MailingListView to display statistics:
      - Total number of mailings
      - Number of active mailings
      - Number of completed mailings

--- mailing/views.py
+++ mailing/views.py
@@ -52,3 +52,10 @@ class MailingListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Mailing.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_mailings"] = Mailing.objects.count()
+        context["started_mailings"] = Mailing.objects.filter(status="started").count()
+        context["finished_mailings"] = Mailing.objects.filter(status="finished").count()
+        return context
